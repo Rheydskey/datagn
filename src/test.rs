8721dc5ab6fe5394f339111b87ca3d1a86566bee
Rheydskey <matgag02@gmail.com>
2021-02-09 20:46:44 +0100

    :white_check_mark: Update test

--- src/test.rs
+++ src/test.rs
@@ -1,23 +1,25 @@
-use crate::config::DatabaseConfig;
-use crate::DatabaseType;
-use logger::{info, error};
-#[test]
-pub fn test() {
-    async_std::task::block_on(
-        async {
-            let file = std::fs::File::create("./db.sql").unwrap();
+mod test {
+    #[test]
+    pub fn test() {
+        use crate::config::DatabaseConfig;
+        use crate::database::DatabaseType;
+        use logger::{error, info};
+
+        async_std::task::block_on(async {
             let mut e = DatabaseConfig {
                 database_type: DatabaseType::Mariadb,
                 ip: "127.0.0.1".to_string(),
                 port: "3306".parse().unwrap(),
                 user: "root".to_string(),
-                password: "Rheydskey123!".to_string()
-            }.to_datapool().await;
-            match e.execute("BEGIN;").await {
-                Ok(e) => info("Working"),
-                Err(e) => error(e)
-            };
-        }
-    );
+                password: "".to_string(),
+            }
+            .to_datapool()
+            .await;
 
+            match e.execute("BEGIN;").await {
+                Ok(_) => info("Working"),
+                Err(e) => error(e),
+            };
+        });
+    }
 }
