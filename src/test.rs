1da19623e57666e39e9659b92d95450105db91e8
Rheydskey <matgag02@gmail.com>
2021-02-13 22:38:02 +0100

    ✨ Update test

--- src/test.rs
+++ src/test.rs
@@ -1,13 +1,16 @@
+use logger::info;
+
 #[test]
 pub fn test() {
     use crate::config::DatabaseConfig;
     use crate::database::DatabaseType;
     use logger::{error, info};
+    use sqlx::Row;
 
     async_std::task::block_on(async {
         let mut e = DatabaseConfig {
-            database_type: DatabaseType::Mysql,
-            ip: String::from("127.0.0.1"),
+            database_type: DatabaseType::Sqlite,
+            ip: String::from("./db.sql"),
             port: String::from("3306"),
             user: String::from("root"),
             password: String::from("Rheydskey123!"),
@@ -15,8 +18,19 @@ pub fn test() {
         .to_datapool()
         .await;
 
-        match e.execute("BEGIN;").await {
+        match e.execute("CREATE TABLE IF NOT EXISTS User (
+            name TEXT NOT NULL,
+            password TEXT NOT NULL
+        );").await {
             Ok(_) => info("Working"),
+            Err(e) => error(e),
+        };
+        e.execute("INSERT INTO User(name, password) VALUES('Datagn','Password')").await.expect("Error");
+        match e.execute_and_fetch_one("SELECT * FROM User").await {
+            Ok(result) => {
+                let name : String = result.try_get("name").expect("Error");
+                println!("{}", name);
+            },
             Err(e) => error(e),
         };
     });
